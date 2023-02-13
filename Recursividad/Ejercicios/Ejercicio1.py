c6f793a74db876ac05e36a92caa9ea2803223792
Cristian <cristianfrancoraigosa@gmail.com>
2023-02-12 20:56:29 -0500

    Initial commit

---
+++ Recursividad/Ejercicios/Ejercicio1.py
@@ -0,0 +1,8 @@
+#Ejercicio numero 1: Realizar el factorial de n numero.
+def factorial(numero):
+    if numero == 1:
+        return 1
+    else:
+        return numero * factorial(numero - 1)
+
+print(factorial(5))
